e421a7a46be985186c73a2cc0dc93b17d56917cf
qwseYT <151243320+qwseYT@users.noreply.github.com>
2024-08-13 15:12:32 +0300

    Update counter.php

--- counter.php
+++ counter.php
@@ -1,22 +1,3 @@
-<?php
-$webhook_url = 'https://discord.com/api/webhooks/1272887133434023937/Y7omG1m8BOh70TScHg-2wVd3ACgu_KSiGJZC-S30BagcygHX--lLOcIDlZfa32P1SadF'; // Замените на ваш URL webhook
-
-$ip_address = $_SERVER['REMOTE_ADDR'];
-
-$data = array(
-    "content" => "Новый пользователь! IP: " . $ip_address
-);
-
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/json\r\n",
-        'method'  => 'POST',
-        'content' => json_encode($data),
-    ),
-);
-
-$context  = stream_context_create($options);
-$result = file_get_contents($webhook_url, false, $context);
 
 if ($result === FALSE) {
 }
